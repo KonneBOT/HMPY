31d639c2bae0cd63f8c33e3d04d74e19c0808c1c
Konne <50216303+KonneBOT@users.noreply.github.com>
2025-12-02 07:18:04 +0100

    Change Autoescape to False again to enable easier XSS

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.jinja_env.autoescape = True
-
+app.jinja_env.autoescape = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buguser:Heute000@localhost/zugfahrten'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_LOGIN_ATTEMPTS'] = 5
